ebfcf0115b7368ed30296fefd60da84e00625ff5
Nicolai Höyer Christiansen <nico388a@edu.easj.dk>
2019-12-03 14:38:31 +0100

    Tilføjet flere klasser

--- RagnarockApp/RagnarockApp/UserNicolai/View/EditUserPage.xaml.cs
+++ RagnarockApp/RagnarockApp/UserNicolai/View/EditUserPage.xaml.cs
@@ -20,9 +20,9 @@ namespace RagnarockApp.UserNicolai.View
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class UserPage : Page
+    public sealed partial class EditUserPage : Page
     {
-        public UserPage()
+        public EditUserPage()
         {
             this.InitializeComponent();
         }
--- RagnarockApp/RagnarockApp/UserNicolai/ViewModel/EditUserViewModel.cs
+++ RagnarockApp/RagnarockApp/UserNicolai/ViewModel/EditUserViewModel.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace RagnarockApp.UserNicolai.ViewModel
 {
-    class UserViewModel
+    class EditUserViewModel
     {
     }
 }
