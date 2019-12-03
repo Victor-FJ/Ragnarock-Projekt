fb49b05468fc10e10945511f7c43e48074aaa44c
Victor Friis-Jensen <vict5481@edu.easj.dk>
2019-12-03 12:14:22 +0100

    Organisering af mapper

--- RagnarockApp/RagnarockApp/MainPage.xaml.cs
+++ RagnarockApp/RagnarockApp/MainPage.xaml.cs
@@ -2,7 +2,7 @@
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
-namespace RagnarockApp.View
+namespace RagnarockApp
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
