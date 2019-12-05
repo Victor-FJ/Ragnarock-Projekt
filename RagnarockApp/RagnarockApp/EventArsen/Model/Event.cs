f62bdb93c6ef987a335825847fed079aa30b5368
Arsen Niyonzima <arse0012@edu.easj.dk>
2019-12-05 12:14:05 +0100

    EventArsen: Model og ViewModel

--- RagnarockApp/RagnarockApp/EventArsen/Model/Event.cs
+++ RagnarockApp/RagnarockApp/EventArsen/Model/Event.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace RagnarockApp.EventArsen.Model
 {
-    class Event
+    public class Event
     {
+        public string Name { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public string Factor { get; set; }
+
+        public Event()
+        {
+            Name = "";
+            Date = "";
+            Time = "";
+            Factor = "";
+        }
+
+        public Event(string name, string date, string time, string factor)
+        {
+            Name = name;
+            Date = date;
+            Time = time;
+            Factor = factor;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} {Date} {Time} {Factor}";
+        }
     }
 }
