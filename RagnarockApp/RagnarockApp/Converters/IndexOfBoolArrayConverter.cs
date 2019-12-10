80e30022e6c136e3495d692d3c35cf1f247eea36
Victor Friis-Jensen <vict5481@edu.easj.dk>
2019-12-10 14:36:58 +0100

    Made changes to editquistion classes

--- RagnarockApp/RagnarockApp/Converters/IndexOfBoolArrayConverter.cs
+++ RagnarockApp/RagnarockApp/Converters/IndexOfBoolArrayConverter.cs
@@ -1,16 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using Windows.UI.Xaml.Data;
 
 namespace RagnarockApp.Converters
 {
-    public class IndexOfArrayConverter : IValueConverter
+    public class IndexOfBoolArrayConverter : IValueConverter
     {
-        private object[] _array;
+        private bool[] _array;
         public object Convert(object value, Type targetType, object parameter, string language)
         {
             // The value parameter is the data from the source object.
-            _array = value as object[];
-            int index = Int32.Parse((string) parameter);
+            _array = value as bool[];
+            int index = Int32.Parse((string)parameter);
             if (_array == null)
                 return null;
             return _array[index];
@@ -22,7 +26,7 @@ namespace RagnarockApp.Converters
             if (_array == null)
                 return null;
             int index = Int32.Parse((string)parameter);
-            _array[index] = value;
+            _array[index] = (bool) value;
             return _array;
         }
     }
