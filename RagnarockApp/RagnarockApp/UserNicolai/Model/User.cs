1c89ddaf61700a0549bd50131fdae2e5d12ea6a7
Nicolai Höyer Christiansen <nico388a@edu.easj.dk>
2019-12-12 09:28:06 +0100

    små tilføjelser til mine klasser

--- RagnarockApp/RagnarockApp/UserNicolai/Model/User.cs
+++ RagnarockApp/RagnarockApp/UserNicolai/Model/User.cs
@@ -55,6 +55,7 @@ namespace RagnarockApp.UserNicolai.Model
         //Constructor
         public User()
         {
+
         }
 
         public User(string name, int id, bool administrator, string userName, int code)
