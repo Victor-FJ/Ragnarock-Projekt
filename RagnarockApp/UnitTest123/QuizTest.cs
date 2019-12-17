624f3322f05e5abdbda15e695be50b4e2171ffcd
Victor Friis-Jensen <vict5481@edu.easj.dk>
2019-12-17 13:17:03 +0100

    Test changes

---
+++ RagnarockApp/UnitTest123/QuizTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RagnarockApp.UserNicolai.Model;
+
+namespace UnitTest123
+{
+    [TestClass]
+    public class QuizTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+            Assert.AreEqual(1, 1);
+
+        }
+    }
+}
